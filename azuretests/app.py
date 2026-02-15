9f167ea6bced6ef74549a330175cb07fb1f5a198
kfirbio <kfir.irani@biocatch.com>
2026-02-15 18:12:02 +0200

    .

--- azuretests/app.py
+++ azuretests/app.py
@@ -13,6 +13,10 @@ from fastapi import FastAPI
 
 from azuretests.env_vars import env_name_ev, metric_send_every_override_ev
 from azuretests.loader import start_load
+from env_vars import  azure_share_name_ev, connection_string_ev
+
+from env_vars import azure_share_name_ev
+from azure.storage.fileshare.aio import ShareServiceClient
 
 
 logger = getLogger("biocatch." + __name__)
@@ -37,7 +41,19 @@ class AzureTestsService(FastAPI):
         setup_logging(logging_settings)
         self.metrics = MetricCollector.from_env()
         await self.metrics.connect(metric_send_every_override_ev.get())
-        # start_load()
+        connection_string = connection_string_ev.get()
+        service_client = ShareServiceClient.from_connection_string(connection_string)
+        share_name = azure_share_name_ev.get()
+        directory_name = "load_tests"
+
+        print("calling get_share_client")
+        share_client = service_client.get_share_client(share_name)
+        print("called get_share_client")
+
+        dir_client = share_client.get_directory_client(directory_name)
+
+
+        await start_load(dir_client, self.metrics)
 
     async def shutdown(self):
         tasks = [self.metrics.close()]
